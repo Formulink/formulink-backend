e384f9b4cc278b6646e6137e3be3dc7a4a8f4725
laynexx <argo150808@gmail.com>
2025-04-10 00:25:09 +0300

    feat: basic main and service implementation

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,13 @@
 package main
 
-import "formulink-backend/internal/service"
+import (
+	"formulink-backend/internal/service"
+	"formulink-backend/pkg/logger"
+)
 
 func main() {
+	logger.Init()
+
 	svc := service.NewService()
 	svc.StartServer()
 }
--- internal/service/service.go
+++ internal/service/service.go
@@ -1,21 +1,32 @@
 package service
 
 import (
+	"formulink-backend/pkg/logger"
 	"github.com/labstack/echo/v4"
 	"net/http"
 )
 
 type Service struct {
+	e *echo.Echo
 }
 
 func NewService() *Service {
 	return &Service{}
 }
 
-func (s *Service) StartServer() {
+func (s *Service) ConfigureEcho() {
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello world!")
 	})
-	e.Logger.Fatal(e.Start(":8080"))
+
+	s.e = e
+}
+
+func (s *Service) StartServer() {
+	if s.e == nil {
+		logger.Lg().Fatal("can't start server because it didn't configured")
+		return
+	}
+	s.e.Logger.Fatal(s.e.Start(":8081"))
 }
