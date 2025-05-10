d1f1087c55076292b1288cdf0f3de315064a50c6
laynexx <argo150808@gmail.com>
2025-05-11 02:37:40 +0300

    fixed getting all formulas

--- internal/service/handler/formulas/formula-handler.go
+++ internal/service/handler/formulas/formula-handler.go
@@ -30,53 +30,91 @@ func NewFormulaHandler(_db *sql.DB, _redis *redis.Client) *FormulaHandler {
 	}
 }
 
+//func (fh *FormulaHandler) GetAllFormulas(c echo.Context) error {
+//
+//	ctx := context.TODO()
+//	v, err := utils.Parse(fh.redis.Get(ctx, "fall").Bytes())
+//	if err != nil {
+//
+//		var formulas []model.Formula
+//		query := `SELECT * from formulas`
+//		rows, err := fh.db.Query(query)
+//		if err != nil {
+//			return c.JSON(http.StatusInternalServerError, err)
+//		}
+//
+//		for rows.Next() {
+//			var formula model.Formula
+//			err = rows.Scan(
+//				&formula.Id,
+//				&formula.SectionId,
+//				&formula.Name,
+//				&formula.Description,
+//				&formula.Expression,
+//				pq.Array(&formula.Parameters),
+//				&formula.Difficulty,
+//				&formula.VideoLink,
+//				&formula.VideoName,
+//			)
+//			if err != nil {
+//				logger.Lg().Logf(0, "can't parse data form db | err: %v", err)
+//				return c.JSON(http.StatusInternalServerError, err)
+//			}
+//			formulas = append(formulas, formula)
+//		}
+//
+//		go func() {
+//			var bytes []byte
+//			if bytes, err = json.Marshal(formulas); err != nil {
+//				return
+//			}
+//			if err = fh.redis.Set(ctx, "fall", bytes, time.Hour).Err(); err != nil {
+//			}
+//		}()
+//
+//		return c.JSON(http.StatusOK, formulas)
+//
+//	}
+//
+//	return c.JSON(http.StatusOK, v)
+//}
+
 func (fh *FormulaHandler) GetAllFormulas(c echo.Context) error {
 
-	ctx := context.TODO()
-	v, err := utils.Parse(fh.redis.Get(ctx, "fall").Bytes())
-	if err != nil {
+	var formulas []model.Formula
 
-		var formulas []model.Formula
-		query := `SELECT * from formulas`
-		rows, err := fh.db.Query(query)
+	query := `SELECT * FROM formulas`
+	rows, err := fh.db.Query(query)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var formula model.Formula
+		err = rows.Scan(
+			&formula.Id,
+			&formula.SectionId,
+			&formula.Name,
+			&formula.Description,
+			&formula.Expression,
+			pq.Array(&formula.Parameters),
+			&formula.Difficulty,
+			&formula.VideoLink,
+			&formula.VideoName,
+		)
 		if err != nil {
+			logger.Lg().Logf(0, "can't parse data form db | err: %v", err)
 			return c.JSON(http.StatusInternalServerError, err)
 		}
-
-		for rows.Next() {
-			var formula model.Formula
-			err = rows.Scan(
-				&formula.Id,
-				&formula.SectionId,
-				&formula.Name,
-				&formula.Description,
-				&formula.Expression,
-				pq.Array(&formula.Parameters),
-				&formula.Difficulty,
-				&formula.VideoLink,
-				&formula.VideoName,
-			)
-			if err != nil {
-				logger.Lg().Logf(0, "can't parse data form db | err: %v", err)
-				return c.JSON(http.StatusInternalServerError, err)
-			}
-			formulas = append(formulas, formula)
-		}
-
-		go func() {
-			var bytes []byte
-			if bytes, err = json.Marshal(formulas); err != nil {
-				return
-			}
-			if err = fh.redis.Set(ctx, "fall", bytes, time.Hour).Err(); err != nil {
-			}
-		}()
-
-		return c.JSON(http.StatusOK, formulas)
-
+		formulas = append(formulas, formula)
 	}
 
-	return c.JSON(http.StatusOK, v)
+	if err = rows.Err(); err != nil {
+		return c.JSON(http.StatusInternalServerError, err)
+	}
+
+	return c.JSON(http.StatusOK, formulas)
 }
 
 func (fh *FormulaHandler) GetFormulasBySectionId(c echo.Context) error {
@@ -215,4 +253,3 @@ func (fh *FormulaHandler) setRandomFormula() error {
 	logger.Lg().Logf(zapcore.InfoLevel, "item succesfully updated in redis")
 	return nil
 }
-
